5b56cb8b9e43adc5cee05d04a739e54ee7b39a13
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-03-13 14:24:56 -0300

    Adicionando projeto

---
+++ Primeiro_APP/Controllers/AlunoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Primeiro_APP.Controllers
+{
+    public class AlunoController : Controller
+    {
+        [ViewData] public string nome { get; set; }        
+        [ViewData] public string sobrenome { get; set; }
+        
+        public IActionResult Index()
+        {
+            ViewBag.nota = "Aluno nota 10";
+            ViewData["Escola"] = "Escola Pedro Fonseca";
+
+            nome = "Nilson";
+            sobrenome = "Alcides";      
+
+            return View();
+        }
+
+        public IActionResult Teste()
+        {
+            TempData["MSG_EX"] = "Mensagem do TempData";
+            return RedirectToAction("Resultado");
+        }
+        public IActionResult Resultado() 
+        {
+            if (TempData["MSG_EX"] != null)
+                ViewBag.Exemplo = "Alunos do 3°B";            
+            return View();
+        }
+       public IActionResult Redireciomaneto()
+        {
+            return Redirect("https://www.google.com/");
+        } 
+
+        public IActionResult RedireciomanetoToRota()
+        {
+            return RedirectToRoute(new { Controller = "Aluno", Action = "Resultado" });
+        }
+        public IActionResult RedireciomanetoPorAction()
+        {
+            return RedirectToAction("Resultado", "Aluno");
+        }
+
+    }
+}
---
+++ Primeiro_APP/Controllers/ProfessorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Primeiro_APP.Controllers
+{
+    public class ProfessorController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.nomeProf = "Nilson J. Alcides";
+            ViewBag.curso = "Desenvolvimento de sistema";
+            ViewBag.disciplina = "Programação Back-end";
+            return View();
+        }
+    }
+}
